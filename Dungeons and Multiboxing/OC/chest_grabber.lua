f145bd3a09ab42461a2d7e6ba1071f66eed11d0f
davoodinator <135515620+McVaxius@users.noreply.github.com>
2025-07-01 09:36:04 -0400

    its coffers and there are 3 i think
    its coffers and there are 3 i think

--- Dungeons and Multiboxing/OC/chest_grabber.lua
+++ Dungeons and Multiboxing/OC/chest_grabber.lua
@@ -7,15 +7,22 @@ functionsToLoad()
 grabbyhands = 1
 sidechest = 0
 
+nemm = {
+	"Bronze Treasure Coffer",
+	"Silver Treasure Coffer",
+	"Gold Treasure Coffer"
+}
+
 while grabbyhands == 1 do
 	if GetCharacterCondition(26) == false then
-		nemm = "Treasure Chest"
-		sidechest = mydistto(GetObjectRawXPos(nemm),GetObjectRawYPos(nemm),GetObjectRawZPos(nemm))
-		if sidechest > 0 and sidechest < 10 then
-			yield("/vnavmesh moveto "..GetObjectRawXPos(nemm).." "..GetObjectRawYPos(nemm).." "..GetObjectRawZPos(nemm))
-			yield("/target \"Treasure Chest\"")
-			if sidechest < 2 then
-				yield("/interact")
+		for i=1,#nemm do
+			sidechest = mydistto(GetObjectRawXPos(nemm[i]),GetObjectRawYPos(nemm[i]),GetObjectRawZPos(nemm[i]))
+			if sidechest > 0 and sidechest < 10 then
+				yield("/vnavmesh moveto "..GetObjectRawXPos(nemm[i]).." "..GetObjectRawYPos(nemm[i]).." "..GetObjectRawZPos(nemm[i]))
+				yield("/target \""..nemm[i].."\"")
+				if sidechest < 2 then
+					yield("/interact")
+				end
 			end
 		end
 	end
