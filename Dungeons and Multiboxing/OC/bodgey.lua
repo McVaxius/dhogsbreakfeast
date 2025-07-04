ba6b70632e1f0328ba6547f7ea545d49ecd22c81
davoodinator <135515620+McVaxius@users.noreply.github.com>
2025-07-04 17:21:48 -0400

    almost a full rewrite
    almost a full rewrite

--- Dungeons and Multiboxing/OC/bodgey.lua
+++ Dungeons and Multiboxing/OC/bodgey.lua
@@ -4,8 +4,7 @@ dodgey_bodger = true
 
 while dodgey_bodger do
 	yield("/wait 5")
-	if Svc.Condition[4] == true or Svc.Condition[10] == true then
+	if (Svc.Condition[4] == true or Svc.Condition[10] == true) and IPC.vnavmesh.IsRunning() then
 		yield("/gaction jump")
-		--yield("/echo wheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee . i am jumping !!!!!!!!!!!!!.")
 	end
 end
