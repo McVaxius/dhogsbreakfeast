245056a69759e109fee9623598c7778d5a59d68f
davoodinator <135515620+McVaxius@users.noreply.github.com>
2025-07-04 16:04:49 -0400

    there i fixed pathing to the bird and such
    there i fixed pathing to the bird and such

---
+++ Dungeons and Multiboxing/OC/bodgey.lua
@@ -0,0 +1,11 @@
+--extremely complex BOCHI helper script to assist passing over obstacles
+
+dodgey_bodger = true
+
+while dodgey_bodger do
+	yield("/wait 5")
+	if Svc.Condition[4] == true or Svc.Condition[10] == true then
+		yield("/gaction jump")
+		yield("/echo wheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee . i am jumping !!!!!!!!!!!!!.")
+	end
+end
