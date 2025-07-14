f834100d71bd871356dad7cef34fcf4b68159ae5
davoodinator <135515620+McVaxius@users.noreply.github.com>
2025-07-14 19:47:18 -0400

    why are non captains turning on que
    why are non captains turning on que

--- Dungeons and Multiboxing/G.O.O.N/Generally Ordered Optimized Navigation.lua
+++ Dungeons and Multiboxing/G.O.O.N/Generally Ordered Optimized Navigation.lua
@@ -178,12 +178,12 @@ decucounter = 0
 --ipc, upc, we all p for c
 if imthecaptainnow == 0 then
 	yield("/echo I'm not the captain now")
+	if IPC.Automaton.IsTweakEnabled("AutoQueue") == true then IPC.Automaton.SetTweakState("AutoQueue", false) end
 end
 if imthecaptainnow == 1 then
 	yield("/echo I'm the captain now")
 	if IPC.Automaton.IsTweakEnabled("AutoQueue") == false then IPC.Automaton.SetTweakState("AutoQueue", true) end
 end
-if IPC.Automaton.IsTweakEnabled("AutoQueue") == true and imthecaptainnow == 0 then IPC.Automaton.SetTweakState("AutoQueue", false) end
 if IPC.Automaton.IsTweakEnabled("EnhancedDutyStartEnd") == false then IPC.Automaton.SetTweakState("EnhancedDutyStartEnd", true) end
 
 function force_rotation()
@@ -222,7 +222,7 @@ while 1 == 1 do
 	yield("/wait 2") --the big wait. run the entire fucking script every 2 seconds
 	checking_the_duct_tape = checking_the_duct_tape + 1
 	
-if IPC.Automaton.IsTweakEnabled("AutoQueue") == false and (duty_counter > 0 or decucounter > 0) then
+if IPC.Automaton.IsTweakEnabled("AutoQueue") == false and (duty_counter > 0 or decucounter > 0) and imthecaptainnow == 1 then
 	yield("/echo Turning ->ON<- Auto Queue -> It was off for some reason")
 	IPC.Automaton.SetTweakState("AutoQueue", true)
 end
