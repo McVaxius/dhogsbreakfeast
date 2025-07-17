11676e822b37dcad93126d06b2fe90921707f1b7
davoodinator <135515620+McVaxius@users.noreply.github.com>
2025-07-17 01:35:59 -0400

    opening MGP
    opening MGP

--- Dungeons and Multiboxing/G.O.O.N/openMGPtickets.lua
+++ Dungeons and Multiboxing/G.O.O.N/openMGPtickets.lua
@@ -1,8 +1,20 @@
 require("dfunc")
 
+yield ("/echo Opening MGP tickets until we have ~ 9.9M")
+
+dMGP = 0
+mMGP = 9900000
+
 if type(GetItemCount(16784)) == "number" then
-	while GetItemCount(16784) > 0 and (Svc.Condition[34] == false or Svc.Condition[26] == false) do
+	while GetItemCount(16784) > 0 and GetItemCount(29) < mMGP and (Svc.Condition[34] == false or Svc.Condition[26] == false) do
 		Inventory.GetInventoryItem(tonumber(16784)):Use()
+		xMGP = GetItemCount(29)
+		if dMGP < xMGP then 
+			yield("/echo Current MGP -> "..xMGP)
+			dMGP = xMGP
+		end
 		yield("/wait 0.5")
 	end
 end
+
+yield("/echo Current MGP -> "..GetItemCount(29))
