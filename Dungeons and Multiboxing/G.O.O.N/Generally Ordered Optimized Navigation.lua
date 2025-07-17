9f93704a116fcb354e8db96d642f064e7545cc21
davoodinator <135515620+McVaxius@users.noreply.github.com>
2025-07-17 08:30:57 -0400

    base level debug stuff
    base level debug stuff

--- Dungeons and Multiboxing/G.O.O.N/Generally Ordered Optimized Navigation.lua
+++ Dungeons and Multiboxing/G.O.O.N/Generally Ordered Optimized Navigation.lua
@@ -390,6 +390,7 @@ if type(Svc.Condition[34]) == "boolean" and type(Svc.Condition[26]) == "boolean"
 					goatfucker = Entity.Target.Name or "goatfucker"
 					if (goatfucker == "Nero tol Scaeva" or goatfucker == "Gaius van Baelsar" or goatfucker == "Phantom Gaius" or goatfucker == "Mark II magitek colossus") and Svc.Condition[26] == true then
 						yield("/vnav stop")
+						if echo_level < 2 then yield("/echo Target in combat condition 26 -> "..goatfucker) end
 					end
 				end
 			end
