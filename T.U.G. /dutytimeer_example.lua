e4d68d2e8f9edba35a91bac816315bb56f107743
davoodinator <135515620+McVaxius@users.noreply.github.com>
2025-08-01 11:28:13 -0400

    whoops hehe
    av

--- T.U.G. /dutytimeer_example.lua
+++ T.U.G. /dutytimeer_example.lua
@@ -8,15 +8,18 @@ require("dfunc")
 function OnDutyCompleted()
 	stop_gooning = 1
 	--math is hard as a rock
-	if Svc.Condition[34] and Svc.Condition[26] == false then
-		yield("/target Ultima")
-	end
 	TAS = (duty_duration * 60) - math.floor(InstancedContent.ContentTimeLeft)
 end
 
 while im_a_cuck do
     yield("/send KEY_1")  --this is here for UC. comment it out if your using something else
     yield("/wait 0.1")
+	if Svc.Condition[34] and Svc.Condition[26] == false then
+		if Svc.ClientState.TerritoryType == 1048 then --decumana
+			yield("/target Ultima")
+			yield("/send KEY_1")
+		end
+	end
 	if stop_gooning == 1 then
 		stop_gooning = 0
 		TAS_mm_ss = secondsToMinutesString(TAS)
