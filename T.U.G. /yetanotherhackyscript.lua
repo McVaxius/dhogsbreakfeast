fa5484dce0688eec5fd176644ef019c7a7b2d2a6
davoodinator <135515620+McVaxius@users.noreply.github.com>
2025-08-24 15:59:57 -0400

    waow
    waow

--- T.U.G. /yetanotherhackyscript.lua
+++ T.U.G. /yetanotherhackyscript.lua
@@ -1,14 +1,18 @@
 --bad questionable helper
+require("dfunc")
 hehe = "heehee"
 while hehe == "heehee" do
-	yield("/bmrai on")
-	if Svc.Condition[34] ~= nil and Svc.Condition[34] == true then
-		yield("/bmrai setpresetname Autoduty Passive")
-		yield("/rotation auto")
+    yield("/bmrai on")
+    if Svc.Condition[34] ~= nil and Svc.Condition[34] == true then
+        yield("/bmrai setpresetname Autoduty Passive")
+        yield("/rotation auto")
+    end
+    if Svc.Condition[34] ~= nil and Svc.Condition[34] == false then
+        yield("/bmrai setpresetname FRENRIDER")
+        yield("/rotation cancel")
+    end
+	if Player.Job.Level > 18 then --make sure we not near training dummy stuff near start of msq
+		yield("/send KEY_1")
 	end
-	if Svc.Condition[34] ~= nil and Svc.Condition[34] == false then
-		yield("/bmrai setpresetname FRENRIDER")
-		yield("/rotation cancel")
-	end
-	yield("/wait 10")
+    yield("/wait 10")
 end
