2ef9c9b9219874ca5576b56f49f7c5b05c92f1a1
davoodinator <135515620+McVaxius@users.noreply.github.com>
2025-08-28 10:57:21 -0400

    hmm its almost usable now
    hmm its almost usable now

--- T.U.G. /yetanotherhackyscript.lua
+++ T.U.G. /yetanotherhackyscript.lua
@@ -1,6 +1,11 @@
 --bad questionable helper
 require("dfunc")
 hehe = "heehee"
+
+hehex = GetPlayerRawXPos()
+hehey = GetPlayerRawYPos()
+hehez = GetPlayerRawZPos()
+
 while hehe == "heehee" do
     yield("/bmrai on")
     if Svc.Condition[34] ~= nil and Svc.Condition[34] == true then
@@ -10,6 +15,19 @@ while hehe == "heehee" do
     if Svc.Condition[34] ~= nil and Svc.Condition[34] == false then
         yield("/bmrai setpresetname FRENRIDER")
         yield("/rotation cancel")
+		
+		--debug
+		--yield("/echo GetPlayerRawXPos() -> "..GetPlayerRawXPos())
+		--yield("/echo hehex -> "..hehex)
+		if GetPlayerRawXPos() - hehex and GetPlayerRawYPos() == hehey and GetPlayerRawZPos() == hehez then
+			yield("/qst stop")
+			yield("/wait 3")
+			yield("/qst start")
+		end
+		
+		hehex = GetPlayerRawXPos()
+		hehey = GetPlayerRawYPos()
+		heehz = GetPlayerRawZPos()
     end
 	if Player.Job.Level > 18 then --make sure we not near training dummy stuff near start of msq
 		yield("/send KEY_1")
