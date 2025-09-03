757c9f9299774ecf50015bfaaa7ffc34da6c15ec
davoodinator <135515620+McVaxius@users.noreply.github.com>
2025-09-03 17:10:55 -0400

    -icon

--- T.U.G. /example of making a popup window in SND.lua
+++ T.U.G. /example of making a popup window in SND.lua
@@ -3,9 +3,9 @@ yield("/wait 0.3")
 yield("/callback CircleList true 3")
 yield("/wait 0.3")
 --fcSizeL = Addons.GetAddon("MultipleHelpWindow"):GetNode(1, 3, 2, 3, 4).Text
---yield("/echo adadf -> "..fcSizeL)
+--yield("/echo adadf -> "..fcSizeL) --this will fail because more than 500 chars
 
-Addons.GetAddon("MultipleHelpWindow"):GetNode(1, 3, 2, 3, 4).Text = "Large Buttfucks\r\r\uE0BB\r\rThank you for choosing the large buttfucks script. now i can\r provide some text here up to 500 characters apparently\r before SND complains and breaks.\r\r OR. Perhaps maybe not. seems I can't PULL more than 500 \rchars but i can shove more if i want.\r\r Formatting for now seems impossible also."
+Addons.GetAddon("MultipleHelpWindow"):GetNode(1, 3, 2, 3, 4).Text = "Large Buttfucks\r\rThank you for choosing the large buttfucks script. now i can\r provide some text here up to 500 characters apparently\r before SND complains and breaks.\r\r OR. Perhaps maybe not. seems I can't PULL more than 500 \rchars but i can shove more if i want.\r\r Formatting for now seems impossible also."
 Addons.GetAddon("MultipleHelpWindow"):GetNode(1, 2, 2, 2).Text = "Large Buttfucks - Help"
 Addons.GetAddon("MultipleHelpWindow"):GetNode(1, 5, 2, 3).Text = "Large Buttfucks"
 
