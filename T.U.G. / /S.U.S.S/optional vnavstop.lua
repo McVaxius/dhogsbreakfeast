44e1ff539a15adbf09d4521506cb0404ecb15040
davoodinator <135515620+McVaxius@users.noreply.github.com>
2026-04-28 18:30:21 -0400

    asdf
    asdf

--- T.U.G. / /S.U.S.S/optional vnavstop.lua
+++
@@ -1,6 +0,0 @@
---optional vnav stop on falling/jumping with new-snd
-if Svc.Condition[48] then 
---yield("/vnav stop")
-IPC.vnavmesh.Stop()
-yield("/echo stopping vnav while we jump")
- end
--- T.U.G. / /S.U.S.S/zzzzz.lua
+++
@@ -1,8 +0,0 @@
---testing pvp nodetext
-
-text1 = GetNodeText("PvPMKSPartyList3", 6)
-yield("/echo Party Member 1 -> "..text1)
-yield("/echo Party Member 2 -> ")
-yield("/echo Party Member 3 -> ")
-yield("/echo Party Member 4 -> ")
-yield("/echo Party Member 5 -> ")
